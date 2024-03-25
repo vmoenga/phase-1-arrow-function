258f435defda5ef7233ece5671ff4310c5ff9787
Victor <vmoenga36@gmail.com>
2024-03-25 16:40:03 +0300

    add new

---
+++ index.js
@@ -0,0 +1,23 @@
+// Define an arrow function called 'divide'
+// Parameters: a, b
+// Description: Divides 2000 by 100
+let divide = (a, b) => 2000 / 100;
+
+// Test the 'divide' function by calling it without arguments
+console.log(divide()); // Output: 20
+
+// Define an arrow function called 'square'
+// Parameters: a
+// Description: Returns the square of the input parameter
+let square = (a) => a * a;
+
+// Test the 'square' function by calling it with an argument
+console.log(square(5)); // Output: 25
+
+// Define an arrow function called 'add'
+// Parameters: a, b
+// Description: Adds 3 and 4 together
+let add = (a, b) => 3 + 4;
+
+// Test the 'add' function by calling it without arguments
+console.log(add()); // Output: 7
